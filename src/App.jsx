9cd0e446ce9bcad2e14839a4b5e0a1828dd9ca4b
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-07-28 23:48:23 +0530

    Add App component of AI-web-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import UserInterface from "./components/UserInterface";
+import './index.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <UserInterface />
+    </>
+  )
+}
+
+export default App
